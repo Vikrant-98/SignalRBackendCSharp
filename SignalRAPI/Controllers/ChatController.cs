5ec40f88029c959b23093571a434b8d85a03d7b3
Vikrant Chitte <vikrant@easyrewardz.com>
2022-11-11 18:45:21 +0530

    develpoment

---
+++ SignalRAPI/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRAPI.Services;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SignalRAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+
+        private readonly ChatHub _services;
+
+        public ChatController(ChatHub services) 
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        [Route("ChatSignal")]
+        public async Task ChatSignal(string message)
+        {
+            await _services.SendMessageToAll(message).ConfigureAwait(false);
+        }
+    }
+}
---
+++ SignalRAPI/Services/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Microsoft.AspNetCore.SignalR;
+using Abp.Dependency;
+using Abp.Runtime.Session;
+
+namespace SignalRAPI.Services
+{
+    public class ChatHub : Hub , ITransientDependency
+    {
+        public IAbpSession AbpSession { get; set; }
+        protected IHubContext<ChatHub> _context;
+        public ChatHub(IHubContext<ChatHub> context) 
+        {
+            _context = context;
+        }
+
+        public async Task SendMessageToAll(string message)
+        {
+            await _context.Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
